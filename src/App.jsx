85455b138170d6963c96b22f42b74a8b0198ced6
booneytan <73635818+booneytan@users.noreply.github.com>
2024-07-31 22:53:00 +0800

    Create Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
-    <div>
+    <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
@@ -21,7 +23,13 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }>
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
@@ -32,7 +40,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </CitiesProvider>
-    </div>
+    </AuthProvider>
   );
 }
 
